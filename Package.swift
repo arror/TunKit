09499401fb06dbf193a633b2b079fe813ebc78e5
arror <arror@users.noreply.github.com>
2023-06-08 10:38:24 +0000

    TunKit Release 0.0.7!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "TunKit",
-      url: "https://github.com/arror/TunKit/releases/download/0.0.6/TunKit.xcframework.zip",
-      checksum: "eecc28f84da2020bab3b2a3b6b07f45dd836d8b11a6c60f5205ebcc9c819819f"
+      url: "https://github.com/arror/TunKit/releases/download/0.0.7/TunKit.xcframework.zip",
+      checksum: "85a27020dd47af9992714549c95c67884d1d8f1179326e70bd4e568e70ae1eef"
     )
   ]
 )
